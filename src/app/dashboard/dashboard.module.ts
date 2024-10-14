09eb6888c47242734365f0b555d1ea6a14ea1b25
Josue952 <josue.zelaya23@itca.edu.sv>
2024-10-14 00:24:54 -0600

    Se implementa el cierre de sesion, un sidebar, y el dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 import { DashboardPage } from './dashboard.page';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { LogoutComponent } from '../components/logout/logout.component';
 
 
 @NgModule({
@@ -17,6 +18,6 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
     DashboardPageRoutingModule,
     
   ],
-  declarations: [DashboardPage, SidebarComponent]
+  declarations: [DashboardPage, SidebarComponent, LogoutComponent]
 })
 export class DashboardPageModule {}
